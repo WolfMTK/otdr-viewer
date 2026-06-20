1e55289e6dcb51c59bc3c45def2f417fa7cd8263
Alexander (wolf) <qrer71@yandex.ru>
2026-06-20 19:17:03 +0300

    Add home page

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-mod app;
+mod ui;
 
-use app::*;
+use crate::ui::component::title_bar::index::TitleBar;
+use crate::ui::page::home::Home;
 use leptos::prelude::*;
+use stylance::import_style;
+
+import_style!(style, "main.module.css");
 
 fn main() {
     console_error_panic_hook::set_once();
     mount_to_body(|| {
         view! {
-            <App/>
+            <div class=style::resize_handle data-tauri-drag-resize-region="n"></div>
+            <div class=style::resize_handle data-tauri-drag-resize-region="s"></div>
+            <div class=style::resize_handle data-tauri-drag-resize-region="w"></div>
+            <div class=style::resize_handle data-tauri-drag-resize-region="e"></div>
+            <div class=style::resize_handle data-tauri-drag-resize-region="nw"></div>
+            <div class=style::resize_handle data-tauri-drag-resize-region="ne"></div>
+            <div class=style::resize_handle data-tauri-drag-resize-region="sw"></div>
+            <div class=style::resize_handle data-tauri-drag-resize-region="se"></div>
+
+            <TitleBar/>
+            <Home/>
         }
     })
 }
