2c2285578181c2bc4a38d1780a1a0da90484690c
Alexander (wolf) <qrer71@yandex.ru>
2026-06-20 19:40:32 +0300

    Add attribute "start_resize"

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,43 @@ mod ui;
 use crate::ui::component::title_bar::index::TitleBar;
 use crate::ui::page::home::Home;
 use leptos::prelude::*;
+use serde::Serialize;
 use stylance::import_style;
+use wasm_bindgen::prelude::wasm_bindgen;
+use wasm_bindgen::JsValue;
 
 import_style!(style, "main.module.css");
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"], js_name = invoke)]
+    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+}
+
+#[derive(Serialize)]
+struct ResizeArgs {
+    direction: &'static str,
+}
+
+fn start_resize(direction: &'static str) {
+    wasm_bindgen_futures::spawn_local(async move {
+        let args = serde_wasm_bindgen::to_value(&ResizeArgs { direction }).unwrap_or(JsValue::NULL);
+        invoke("start_resize", args).await;
+    });
+}
 
 fn main() {
     console_error_panic_hook::set_once();
     mount_to_body(|| {
         view! {
-            <div class=style::resize_handle data-tauri-drag-resize-region="n"></div>
-            <div class=style::resize_handle data-tauri-drag-resize-region="s"></div>
-            <div class=style::resize_handle data-tauri-drag-resize-region="w"></div>
-            <div class=style::resize_handle data-tauri-drag-resize-region="e"></div>
-            <div class=style::resize_handle data-tauri-drag-resize-region="nw"></div>
-            <div class=style::resize_handle data-tauri-drag-resize-region="ne"></div>
-            <div class=style::resize_handle data-tauri-drag-resize-region="sw"></div>
-            <div class=style::resize_handle data-tauri-drag-resize-region="se"></div>
+            <div class=style::resize_handle data-resize-dir="n" on:mousedown=move |_| start_resize("North")></div>
+            <div class=style::resize_handle data-resize-dir="s" on:mousedown=move |_| start_resize("South")></div>
+            <div class=style::resize_handle data-resize-dir="w" on:mousedown=move |_| start_resize("West")></div>
+            <div class=style::resize_handle data-resize-dir="e" on:mousedown=move |_| start_resize("East")></div>
+            <div class=style::resize_handle data-resize-dir="nw" on:mousedown=move |_| start_resize("NorthWest")></div>
+            <div class=style::resize_handle data-resize-dir="ne" on:mousedown=move |_| start_resize("NorthEast")></div>
+            <div class=style::resize_handle data-resize-dir="sw" on:mousedown=move |_| start_resize("SouthWest")></div>
+            <div class=style::resize_handle data-resize-dir="se" on:mousedown=move |_| start_resize("SouthEast")></div>
 
             <TitleBar/>
             <Home/>
