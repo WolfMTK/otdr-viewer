0ce8ebd314eca174ec0b00744c2da394fa8f27b4
Alexander (wolf) <qrer71@yandex.ru>
2026-06-20 19:39:15 +0300

    Add command `start_resize`

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,16 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::Window;
+use tauri_runtime::ResizeDirection;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn start_resize(window: Window, direction: ResizeDirection) {
+    let _ = window.start_resize_dragging(direction);
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![start_resize])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
