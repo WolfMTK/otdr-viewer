8793661918d173009314c4830b4aa9738f0ad37d
Alexander (wolf) <qrer71@yandex.ru>
2026-06-21 20:51:33 +0300

    Add commands `start_resize`, `close_window`, `toggle_maximize`, `minimize_window`

--- src/ui/component/title_bar/index.rs
+++ src/ui/component/title_bar/index.rs
@@ -1,7 +1,28 @@
 use leptos::prelude::*;
 use stylance::import_style;
+use wasm_bindgen::JsValue;
+
+use crate::tauri::invoke;
 
 import_style!(style, "index.module.css");
+
+fn close_window() {
+    wasm_bindgen_futures::spawn_local(async move {
+        invoke("close_window", JsValue::NULL).await;
+    })
+}
+
+fn toggle_maximize() {
+    wasm_bindgen_futures::spawn_local(async move {
+        invoke("toggle_maximize", JsValue::NULL).await;
+    })
+}
+
+fn minimize_window() {
+    wasm_bindgen_futures::spawn_local(async move {
+        invoke("minimize_window", JsValue::NULL).await;
+    })
+}
 
 #[component]
 pub fn TitleBar() -> impl IntoView {
@@ -15,9 +36,12 @@ pub fn TitleBar() -> impl IntoView {
             </div>
             <div data-tauri-drag-region class=style::titlebar_drag></div>
             <div class=style::titlebar_controls>
-                <button id="titlebar-maximize" class=stylance::classes!(style::dot, style::dot_green)></button>
-                <button id="titlebar-minimize" class=stylance::classes!(style::dot, style::dot_yellow)></button>
-                <button id="titlebar-close" class=stylance::classes!(style::dot, style::dot_red)></button>
+                <button class=stylance::classes!(style::dot, style::dot_green)
+                        on:click=move |_| minimize_window()/>
+                <button class=stylance::classes!(style::dot, style::dot_yellow)
+                        on:click=move |_| toggle_maximize()/>
+                <button class=stylance::classes!(style::dot, style::dot_red)
+                        on:click=move |_| close_window()/>
             </div>
         </div>
     }
