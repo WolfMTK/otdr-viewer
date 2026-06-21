13a803e4b4467715592d110e8adc347e82886160
Alexander (wolf) <qrer71@yandex.ru>
2026-06-21 21:53:00 +0300

    Add buttons for menu

--- src/ui/component/title_bar/index.rs
+++ src/ui/component/title_bar/index.rs
@@ -26,15 +26,33 @@ fn minimize_window() {
 
 #[component]
 pub fn TitleBar() -> impl IntoView {
+    let (menu_open, set_menu_open) = signal(false);
+
     view! {
         <div class=style::titlebar>
             <div class=style::titlebar_left>
                 <img src="public/logo.svg" class=style::app_icon alt="logo" draggable="false" />
-                <button class=style::menu_button on:click=|_| {  }>
-                    <img src="public/menu.svg" class=style::menu_icon alt="menu" draggable="false" />
-                </button>
+
+                <Show when=move || !menu_open.get()>
+                    <button class=style::menu_button
+                            on:click=move |_| set_menu_open.set(true)>
+                        <img src="public/menu.svg" class=style::menu_icon alt="menu" draggable="false" />
+                    </button>
+                </Show>
+
+                <Show when=move || menu_open.get()>
+                    <nav class=style::menu_bar>
+                        <button class=style::menu_item>"File"</button>
+                        <button class=style::menu_item>"Edit"</button>
+                        <button class=style::menu_item>"View"</button>
+                        <button class=style::menu_item>"Help"</button>
+                    </nav>
+                </Show>
             </div>
-            <div data-tauri-drag-region class=style::titlebar_drag></div>
+
+            <div data-tauri-drag-region
+                 class=style::titlebar_drag></div>
+
             <div class=style::titlebar_controls>
                 <button class=stylance::classes!(style::dot, style::dot_green)
                         on:click=move |_| minimize_window()/>
@@ -44,5 +62,10 @@ pub fn TitleBar() -> impl IntoView {
                         on:click=move |_| close_window()/>
             </div>
         </div>
+
+        <Show when=move || menu_open.get()>
+            <div class=style::menu_overlay
+                 on:click=move |_| set_menu_open.set(false)></div>
+        </Show>
     }
 }
