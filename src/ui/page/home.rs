233da8a6b922113b3fd32999cb9fdf221a571dbb
Alexander (wolf) <qrer71@yandex.ru>
2026-06-27 16:59:05 +0300

    Add components `StatusBar` and `Dropzone`

--- src/ui/page/home.rs
+++ src/ui/page/home.rs
@@ -3,6 +3,8 @@ use stylance::import_style;
 
 use crate::ui::component::recent_files::index::RecentFiles;
 use crate::ui::component::sidebar::index::Sidebar;
+use crate::ui::component::status_bar::index::StatusBar;
+use crate::ui::component::dropzone::index::Dropzone;
 
 import_style!(style, "home.module.css");
 
@@ -11,10 +13,15 @@ pub fn Home() -> impl IntoView {
     let panel_open = RwSignal::new(true);
 
     view! {
-        <div class=style::layout>
-            <Sidebar panel_open=panel_open/>
-            <RecentFiles panel_open=panel_open/>
-            <main class=style::content></main>
+        <div class=style::screen>
+            <div class=style::layout>
+                <Sidebar panel_open=panel_open/>
+                <RecentFiles panel_open=panel_open/>
+                <main class=style::content>
+                    <Dropzone/>
+                </main>
+            </div>
+            <StatusBar/>
         </div>
     }
 }
