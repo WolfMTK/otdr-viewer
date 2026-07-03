727512805220c32f3a3350a4125e9cb52259084d
Alexander (wolf) <qrer71@yandex.ru>
2026-07-03 21:44:26 +0300

    Add new migrations

---
+++ src-tauri/migrations/20260703180557_update_recent_files.down.sql
@@ -0,0 +1,2 @@
+-- Add down migration script here
+ALTER TABLE recent_files DROP COLUMN length_km;
---
+++ src-tauri/migrations/20260703180557_update_recent_files.up.sql
@@ -0,0 +1,2 @@
+-- Add up migration script here
+ALTER TABLE recent_files ADD COLUMN length_km REAL;
