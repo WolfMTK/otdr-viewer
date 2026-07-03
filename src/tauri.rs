7472d9e1066e388a1be79f69c27963b59a59530a
Alexander (wolf) <qrer71@yandex.ru>
2026-07-03 22:38:43 +0300

    Add functions `invoke_parsed`, `invoke_parsed_with_args`, `invoke_with_args`

--- src/tauri.rs
+++ src/tauri.rs
@@ -1,3 +1,5 @@
+use serde::de::DeserializeOwned;
+use serde::Serialize;
 use wasm_bindgen::prelude::wasm_bindgen;
 use wasm_bindgen::JsValue;
 
@@ -11,4 +13,20 @@ pub fn invoke_fire_and_forget(cmd: &'static str) {
     wasm_bindgen_futures::spawn_local(async move {
         invoke(cmd, JsValue::NULL).await;
     })
+}
+
+fn to_js_args<A: Serialize>(args: &A) -> JsValue {
+    serde_wasm_bindgen::to_value(args).unwrap_or(JsValue::NULL)
+}
+
+pub async fn invoke_parsed<T: DeserializeOwned + Default>(cmd: &str) -> T {
+    serde_wasm_bindgen::from_value(invoke(cmd, JsValue::NULL).await).unwrap_or_default()
+}
+
+pub async fn invoke_parsed_with_args<A: Serialize, T: DeserializeOwned + Default>(cmd: &str, args: &A) -> T {
+    serde_wasm_bindgen::from_value(invoke(cmd, to_js_args(args)).await).unwrap_or_default()
+}
+
+pub async fn invoke_with_args<A: Serialize>(cmd: &str, args: &A) {
+    invoke(cmd, to_js_args(args)).await;
 }
