c668c63ca542490c8e1bbb75eab1b7ae37ff7f65
Alexander (wolf) <qrer71@yandex.ru>
2026-07-07 20:35:38 +0300

    Add structure `OpenedSor`

--- src/ui/context.rs
+++ src/ui/context.rs
@@ -1,7 +1,11 @@
 use leptos::prelude::*;
+use shared_types::SorData;
 
 #[derive(Clone, Copy)]
 pub struct RecentFilesVersion(pub RwSignal<u32>);
+
+#[derive(Clone, Copy)]
+pub struct OpenedSor(pub RwSignal<Option<SorData>>);
 
 pub fn provide_recent_files_version() {
     provide_context(RecentFilesVersion(RwSignal::new(0)));
