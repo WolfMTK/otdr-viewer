c31017ccbbfdd39754f1845036b0571d789f5a0d
Alexander (wolf) <qrer71@yandex.ru>
2026-07-07 20:48:24 +0300

    Update component `Home`

--- src/ui/page/home.rs
+++ src/ui/page/home.rs
@@ -1,10 +1,25 @@
 use leptos::prelude::*;
 
 use crate::ui::component::dropzone::index::Dropzone;
+use crate::ui::context::OpenedSor;
 
 #[component]
 pub fn Home() -> impl IntoView {
+    let OpenedSor(opened) = use_context::<OpenedSor>().expect("OpenedSor is provided at app root");
+
     view! {
-        <Dropzone/>
+        <Show when=move || opened.get().is_none()>
+            <Dropzone/>
+        </Show>
+
+        <Show when=move || opened.get().is_some()>
+            <div>{move || {
+                opened.get().map(|d| format!(
+                    "Открыт файл: {} точек трассы, {} событий",
+                    d.trace.distances_km.len(),
+                    d.summary.events.len(),
+                )).unwrap_or_default()
+            }}</div>
+        </Show>
     }
 }
