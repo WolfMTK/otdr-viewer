11cdbba3cc3e674932abb444b856dcef64cc5515
Alexander (wolf) <qrer71@yandex.ru>
2026-07-12 17:59:07 +0300

    Add structure `CompareMode`

--- src/ui/context.rs
+++ src/ui/context.rs
@@ -7,7 +7,11 @@ pub struct RecentFilesVersion(pub RwSignal<u32>);
 #[derive(Clone, Copy)]
 pub struct OpenedSor(pub RwSignal<Option<SorData>>);
 
+#[derive(Clone, Copy)]
+pub struct CompareMode(pub RwSignal<bool>);
+
 pub fn provide_app_context() {
     provide_context(RecentFilesVersion(RwSignal::new(0)));
     provide_context(OpenedSor(RwSignal::new(None)));
+    provide_context(CompareMode(RwSignal::new(false)));
 }
